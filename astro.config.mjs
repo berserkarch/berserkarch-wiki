5526d2f3163b574ad08e4365beb6731da59aa869
Gaurav Raj <techw803@gmail.com>
2025-06-29 21:16:07 +0530

    Initial Commit

---
+++ astro.config.mjs
@@ -0,0 +1,29 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Berserk Arch',
+			logo: {
+				src: './src/assets/banner-logo.svg',
+			},
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/berserkarch/' }],
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
